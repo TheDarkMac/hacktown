c3aaaf3a41b4c2b3f5857ec67e15ecd9cdc4a23a
Abega1642 <hei.abega.2@gmail.com>
2024-07-28 12:22:16 +0300

    feat: build get all to do list rest api

--- hackathon/src/main/java/com/hei/hackathon/hackathon/controller/ToDoListController.java
+++ hackathon/src/main/java/com/hei/hackathon/hackathon/controller/ToDoListController.java
@@ -1,22 +1,26 @@
 package com.hei.hackathon.hackathon.controller;
 
-
+import com.hei.hackathon.hackathon.dto.ToDoListDto;
+import com.hei.hackathon.hackathon.services.TodoListServices;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+@CrossOrigin("*")
+@AllArgsConstructor
 @RestController
 @RequestMapping("/back/data")
 public class ToDoListController {
+    TodoListServices todoListServices;
     @GetMapping("/todoLists")
-    public List<ToDoList> getToDoLists() {
-
+    public ResponseEntity<List<ToDoListDto>> getToDoLists() {
+        List<ToDoListDto> todoLists = todoListServices.getAllTodoList();
+        System.out.println(todoLists);
+        return ResponseEntity.ok(todoLists);
     }
 }
