932111634bbab8e4e33e7a9af9d51945649197e7
Abega1642 <hei.abega.2@gmail.com>
2024-07-28 21:38:06 +0300

    feat: finalize user service at 70%

--- hackathon/src/main/java/com/hei/hackathon/hackathon/services/UserService.java
+++ hackathon/src/main/java/com/hei/hackathon/hackathon/services/UserService.java
@@ -5,4 +5,12 @@ import java.util.List;
 
 public interface UserService {
      List<UserDto> getAllUsers();
+
+     UserDto getUserById(long id);
+
+     UserDto createUser(UserDto userDto);
+
+     UserDto updateUser(long id, UserDto userDto);
+
+     void deleteUser(long userId);
 }
--- hackathon/src/main/java/com/hei/hackathon/hackathon/services/implementation/UserServiceImpl.java
+++ hackathon/src/main/java/com/hei/hackathon/hackathon/services/implementation/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hei.hackathon.hackathon.services.implementation;
 
 import com.hei.hackathon.hackathon.dto.UserDto;
 import com.hei.hackathon.hackathon.entity.User;
+import com.hei.hackathon.hackathon.exceptions.ResourceNotFoundException;
 import com.hei.hackathon.hackathon.mapper.UserMapper;
 import com.hei.hackathon.hackathon.repository.UserRepository;
 import com.hei.hackathon.hackathon.services.UserService;
@@ -25,5 +26,42 @@ public class UserServiceImpl implements UserService {
         return userList.stream()
                 .map(UserMapper::MapToUserDto)
                 .toList();
+    }
+
+    @Override
+    public UserDto getUserById(long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow( () ->
+                        new ResourceNotFoundException("User with id " + id + " wasn't found !")
+                );
+        return UserMapper.MapToUserDto(user);
+    }
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        User addedUser = userRepository.save(UserMapper.MapToUser(userDto));
+        return UserMapper.MapToUserDto(addedUser);
+    }
+
+    @Override
+    public UserDto updateUser(long id, UserDto userDto) {
+        User user = userRepository.findById(id)
+                .orElseThrow( () ->
+                        new ResourceNotFoundException("User with id " + id + " wasn't found !")
+                );
+
+        user.setUname(userDto.getUname());
+        user.setUserName(userDto.getUserName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(CryptoServiceImpl.encrypts(userDto.getPassword()));
+
+        User updatedUser = userRepository.save(user);
+
+        return UserMapper.MapToUserDto(updatedUser);
+    }
+
+    @Override
+    public void deleteUser(long useId) {
+        userRepository.deleteById(useId);
     }
 }
