a4277d837cf4d3a33511d3044455e9bbed444fb9
Abega1642 <hei.abega.2@gmail.com>
2024-07-28 21:40:42 +0300

    feat: finalize user CRUD rest api

--- hackathon/src/main/java/com/hei/hackathon/hackathon/controller/UserController.java
+++ hackathon/src/main/java/com/hei/hackathon/hackathon/controller/UserController.java
@@ -3,11 +3,9 @@ package com.hei.hackathon.hackathon.controller;
 import com.hei.hackathon.hackathon.dto.UserDto;
 import com.hei.hackathon.hackathon.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,30 @@ public class UserController {
     public ResponseEntity<List<UserDto>> getUsers() {
         List<UserDto> userDtoList = userService.getAllUsers();
         return ResponseEntity.ok(userDtoList);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable long id) {
+        UserDto userDto = userService.getUserById(id);
+        return ResponseEntity.ok(userDto);
+    }
+
+    @PostMapping("/users/add")
+    public ResponseEntity<UserDto> addUser(@RequestBody UserDto userDto) {
+        UserDto addUser = userService.createUser(userDto);
+        return new ResponseEntity<>(addUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/users/update/{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable("id") long id, @RequestBody UserDto userDto) {
+        UserDto updateUser = userService.updateUser(id, userDto);
+        return new ResponseEntity<>(updateUser, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/users/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        userService.deleteUser(id);
+        return "User deleted with success !";
     }
 
 }
