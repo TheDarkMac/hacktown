43bf3342499391bfe1d62700afea6ea3e4553ce3
Abega1642 <hei.abega.2@gmail.com>
2024-07-28 16:13:28 +0300

    feat: add user repository interface

--- hackathon/src/main/java/com/hei/hackathon/hackathon/repository/UserRepository.java
+++ hackathon/src/main/java/com/hei/hackathon/hackathon/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.hei.hackathon.hackathon.repository;
 
-public class UserRepository {
+import com.hei.hackathon.hackathon.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
 }
