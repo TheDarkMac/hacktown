fcc11ee6455b98e4c404d5d401f9333932e7526b
Abega1642 <hei.abega.2@gmail.com>
2024-07-28 21:40:14 +0300

    chore: replace the value String by Long

--- hackathon/src/main/java/com/hei/hackathon/hackathon/repository/UserRepository.java
+++ hackathon/src/main/java/com/hei/hackathon/hackathon/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.hei.hackathon.hackathon.repository;
 import com.hei.hackathon.hackathon.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
