b74c21cf720d4c07d83130dfa84f043a8720074b
Abega1642 <hei.abega.2@gmail.com>
2024-07-28 16:12:48 +0300

    feat: add user service and its implementation

--- hackathon/src/main/java/com/hei/hackathon/hackathon/services/UserService.java
+++ hackathon/src/main/java/com/hei/hackathon/hackathon/services/UserService.java
@@ -1,4 +1,15 @@
 package com.hei.hackathon.hackathon.services;
 
-public class UserService {
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public interface UserService{
+
+    byte[] encryptPassword(String password) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException;
+
+    String decryptPassword(byte[] encryptedPassword) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException;
+
 }
--- hackathon/src/main/java/com/hei/hackathon/hackathon/services/implementation/UserServiceImpl.java
+++ hackathon/src/main/java/com/hei/hackathon/hackathon/services/implementation/UserServiceImpl.java
@@ -1,4 +1,47 @@
 package com.hei.hackathon.hackathon.services.implementation;
 
-public class UserServiecImpl {
+import com.hei.hackathon.hackathon.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+
+@AllArgsConstructor
+@Service
+public class UserServiceImpl implements UserService {
+    final String ALGORITHM = "AES";
+    final byte[] KEY = "capdata2023capdat".getBytes();
+
+    @Override
+    public byte[] encryptPassword(String password)
+            throws NoSuchPaddingException,
+            NoSuchAlgorithmException,
+            InvalidKeyException,
+            IllegalBlockSizeException,
+            BadPaddingException {
+
+        SecretKey secretKey = new SecretKeySpec(KEY, ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+        return cipher.doFinal(password.getBytes());
+    }
+
+    @Override
+    public String decryptPassword(byte[] encryptedPassword)
+            throws NoSuchPaddingException,
+            NoSuchAlgorithmException,
+            InvalidKeyException,
+            IllegalBlockSizeException,
+            BadPaddingException {
+
+        SecretKey secretKey = new SecretKeySpec(KEY, ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        return new String(cipher.doFinal(encryptedPassword));
+    }
 }
