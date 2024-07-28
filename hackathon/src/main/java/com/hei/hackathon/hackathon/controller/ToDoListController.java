2bc16d3c1c17677432f069e775221630d89b435c
Abega1642 <hei.abega.2@gmail.com>
2024-07-28 13:03:48 +0300

    feat: build get by Id & create|add rest api

--- hackathon/src/main/java/com/hei/hackathon/hackathon/controller/ToDoListController.java
+++ hackathon/src/main/java/com/hei/hackathon/hackathon/controller/ToDoListController.java
@@ -1,26 +1,37 @@
 package com.hei.hackathon.hackathon.controller;
 
 import com.hei.hackathon.hackathon.dto.ToDoListDto;
+import com.hei.hackathon.hackathon.entity.ToDoList;
 import com.hei.hackathon.hackathon.services.TodoListServices;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @CrossOrigin("*")
 @AllArgsConstructor
 @RestController
-@RequestMapping("/back/data")
+@RequestMapping("/back/data/todoLists")
 public class ToDoListController {
     TodoListServices todoListServices;
-    @GetMapping("/todoLists")
+
+    @GetMapping
     public ResponseEntity<List<ToDoListDto>> getToDoLists() {
         List<ToDoListDto> todoLists = todoListServices.getAllTodoList();
-        System.out.println(todoLists);
         return ResponseEntity.ok(todoLists);
+    }
+
+    @GetMapping("{toDoListId}")
+    public ResponseEntity<ToDoListDto> getToDoList(@PathVariable("toDoListId") String toDoListId) {
+        ToDoListDto toDoList = todoListServices.getTodoListById(toDoListId);
+        return ResponseEntity.ok(toDoList);
+    }
+
+    @PostMapping
+    public ResponseEntity<ToDoListDto> addToDoList(@RequestBody ToDoListDto toDoListDto) {
+        ToDoListDto addedToDoList = todoListServices.createTodoList(toDoListDto);
+        return new ResponseEntity<>(addedToDoList, HttpStatus.CREATED);
     }
 }
