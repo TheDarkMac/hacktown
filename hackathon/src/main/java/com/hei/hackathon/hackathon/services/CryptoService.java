39a82dbbffb00f9557ae900ba250d77e7b176468
Abega1642 <hei.abega.2@gmail.com>
2024-08-13 19:05:18 +0300

    chore: re-implement crypto service

--- hackathon/src/main/java/com/hei/hackathon/hackathon/services/CryptoService.java
+++ hackathon/src/main/java/com/hei/hackathon/hackathon/services/CryptoService.java
@@ -1,7 +1,5 @@
 package com.hei.hackathon.hackathon.services;
 
 public interface CryptoService {
-    String decrypt(String encrypted);
-
     String encrypt(String decrypted);
 }
--- hackathon/src/main/java/com/hei/hackathon/hackathon/services/implementation/CryptoServiceImpl.java
+++ hackathon/src/main/java/com/hei/hackathon/hackathon/services/implementation/CryptoServiceImpl.java
@@ -1,65 +1,11 @@
 package com.hei.hackathon.hackathon.services.implementation;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class CryptoServiceImpl {
 
-    public static int charCode(char c) {
-        c = Character.toUpperCase(c);
-        return (int) c - 65;
-    }
-
-    public static String encrypts(String decrypted) {
-        String s = decrypted.toUpperCase();
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = decrypted.charAt(i);
-
-            if (Character.isLetter(currentChar)) {
-                boolean isUpperCase =
-                        (String.valueOf(currentChar)
-                                .equals(String.valueOf(currentChar).toUpperCase()));
-
-                int unicode = charCode(currentChar) + 13;
-                unicode = unicode % 26;
-                char res = (char) (unicode + 65);
-
-                if (isUpperCase) {
-                    result.append(res);
-                } else {
-                    result.append(Character.toLowerCase(res));
-                }
-            } else {
-                result.append(currentChar);
-            }
-        }
-        return result.toString();
-    }
-
-    public static String decrypts(String encrypted) {
-        String s = encrypted.toUpperCase();
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = encrypted.charAt(i);
-
-            if (Character.isLetter(currentChar)) {
-                boolean isUpperCase =
-                        (String.valueOf(currentChar)
-                                .equals(String.valueOf(currentChar).toUpperCase()));
-
-                int unicode = charCode(currentChar) + 13;
-                unicode = unicode % 26;
-                char res = (char) (unicode + 65);
-
-                if (isUpperCase) {
-                    result.append(res);
-                } else {
-                    result.append(Character.toLowerCase(res));
-                }
-            } else {
-                result.append(currentChar);
-            }
-        }
-        return result.toString();
+    public static String encrypts(String password) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.encode(password);
     }
 }
